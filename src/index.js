5887bf61a566f6ee7dce38197f3d336ebce8d15f
Peerawich Sukijsopon <peerawich@Peerawichs-MacBook-Pro.local>
2021-07-29 19:22:23 +0700

    modified:   index.js

--- src/index.js
+++ src/index.js
@@ -114,7 +114,7 @@ async function readBlockInterval() {
 
     const [idpRequests, asRequests] = await processPendingRequest(idpCatagorizedRequests, asCatagorizedRequests);
 
-    // if (new Date().getMinutes() === 0) {
+    if (new Date().getMinutes() === 0) {
 
         const requestSuccessRateList = await getRequestSuccessRate(idpRequests, asRequests);
         const nodeIdListToNotify = Object.keys(requestSuccessRateList);
@@ -129,7 +129,7 @@ async function readBlockInterval() {
             }
             await notify.lineNotify(message);
         }
-    // }
+    }
 }
 
 async function groupRequestByNodeId(requestList, role) {
